82e8f9c3c56ebb6edfc4fcefae8e2b2de873f03d
Mitchell Gehring <mail@mitchellgehring.com>
2018-10-10 13:03:03 -0700

    fake data and report page

---
+++ fake_data.py
@@ -0,0 +1,60 @@
+
+manager_data = dict(
+	Communication = 2,
+	Possitive_Conflict = 2, 
+	Psych_Safety = 3,
+	Engagement = 2,
+	Accountability = 1,
+	Clarity = 2,
+	Delegation = 2,
+	Strategic_Reward = 2,
+	Input = 3,
+	Purpose = 3,
+	Trust = 3,
+	Compassion = 1,
+	Fair_Reward = 2,
+	Personal_Growth = 3,
+	Deep_Work = 2,
+	Feedback = 2,
+	Micromanagement = 2,
+	Burn_Out = 2,
+	Employee_Opinion = 1,
+	Well_Being = 1,
+	Turnover_Risk = 1,
+	Leadership = 4,
+	Execution = 2, 
+	Team_Building = 1,
+	People_Development = 5)
+
+company_data = dict(
+	Strategic_Clarity = 3,
+	Engagement = 2,
+	Adaptability = 4, 
+	Psychological_Safety = 1,
+	Communication = 3,
+	Purpose = 3,
+	Productivity = 1, 
+	Leadership = 2,
+	Execution = 3,
+	Team_Buildling = 4, 
+	People_Development = 2, 
+	Micromanagement = 3,
+	Employee_Opinion =1, 
+	Well_Being = 3,
+	Turnover_Risk = 1,
+	Siloed_Information = 2,
+	Innovation_Damage = 3,
+	Burn_Out = 2,
+	Interpersonal_Dysfunction = 4)
+
+def get_manager_data(): 
+	return manager_data
+
+def get_company_data():
+	return company_data
+
+
+
+
+
+
---
+++ manager_report.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import pandas as pd 
+import fake_data 
+
+
+data = fake_data.get_manager_data() 
+
+print(data[1])
+
